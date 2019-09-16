643d0e2b8648864c5f7406496487660a6aeba537
jma224 <43620595+jma224@users.noreply.github.com>
2019-09-15 21:54:30 -0400

    Update SlotMachine.java

--- SlotMachine.java
+++ SlotMachine.java
@@ -1,5 +1,5 @@
-//McGill ID: 260783581
-//Name: Jingyan Ma
+
+//*Jingyan Ma*//
 public class SlotMachine{
    public static void main(String[] args){
      System.out.println("Welcome to Fun Casino!");
